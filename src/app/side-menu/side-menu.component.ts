3e90fb4a67eeab132084dcfab785473225f9f74d
karanshah229 <karanshah229@gmail.com>
2020-06-18 18:14:42 +0530

    Lazy Loaded Modules and States and Union Data

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-side-menu',
-  templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss']
+	selector: 'app-side-menu',
+	templateUrl: './side-menu.component.html',
+	styleUrls: ['./side-menu.component.scss']
 })
 export class SideMenuComponent implements OnInit {
-  @Input() isCollapsed = false;
+	@Input() isCollapsed = false;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
